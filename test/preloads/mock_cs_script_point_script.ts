394f4b0ae408f4e47a0cb6bea60859d80177b361
Benjamin Zaporzan <benzaporzan@gmail.com>
2025-12-29 10:37:32 -0500

    Added point_script mock, tests are passing

---
+++ test/preloads/mock_cs_script_point_script.ts
@@ -0,0 +1,12 @@
+import { mock } from "bun:test";
+
+mock.module("cs_script/point_script", () => {
+    return {
+        Instance: {
+            
+        },
+        Msg: (msg) => console.log(msg),
+        CSPlayerPawn: class CSPlayerPawn {},
+        CSPlayerController: class CSPlayerController {},
+    };
+});
