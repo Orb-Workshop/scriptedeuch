0bef022f5976599a6ea120d981b9ece165de3123
Benjamin Zaporzan <benzaporzan@gmail.com>
2026-01-09 02:59:59 -0500

    Fixed Tests

--- test/preloads/mock_cs_script_point_script.ts
+++ test/preloads/mock_cs_script_point_script.ts
@@ -3,7 +3,8 @@ import { mock } from "bun:test";
 mock.module("cs_script/point_script", () => {
     return {
         Instance: {
-            Msg: (msg) => console.log(msg),  
+            Msg: (msg) => console.log(msg),
+            GetGameTime: _ => 0,
         },
         CSPlayerPawn: class CSPlayerPawn {},
         CSPlayerController: class CSPlayerController {},
