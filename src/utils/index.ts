3fa97d8624502a269aa670931bd3f2df2b30626c
Benjamin Zaporzan <benzaporzan@gmail.com>
2026-01-10 14:04:27 -0500

    More Refactoring

---
+++ src/utils/index.ts
@@ -0,0 +1,57 @@
+/* Utility Functions */
+import {
+    Instance as CSS,
+    CSPlayerPawn,
+    CSPlayerController,
+} from "cs_script/point_script";
+
+export function DeepCopy(o) {
+    return JSON.parse(JSON.stringify(o));
+}
+
+export function GetPlayerName(player_pawn: CSPlayerPawn): string | null {
+    if (!(player_pawn instanceof CSPlayerPawn) ||
+        !player_pawn.IsValid())
+        return null;
+    const player_controller = player_pawn.GetOriginalPlayerController();
+    if (!(player_controller instanceof CSPlayerController) ||
+        !player_controller.IsValid())
+        return null;
+    return player_controller.GetPlayerName();
+}
+
+export const Memoize = (func) => {
+    const cache = {};
+    return (...args) => {
+        let strKey = args.join(',');
+        if(!cache[strKey]){
+            cache[strKey] = func.apply(this, args);
+        }
+        return cache[strKey];
+    }
+}
+
+export function GenerateAddOutput(opts) {
+    const {
+        output_name,
+        target_name,
+        input_name,
+        parameter = "",
+        delay = 0,
+        max = -1, // infinite
+    } = opts;
+    return `${output_name}>${target_name}>${input_name}>${parameter}>${delay}>${max}`;
+}
+
+
+function* UniqueNameGenerator(s) {
+    let id = 0;
+    while(true) {
+        yield (s + id++);
+    }
+}
+
+export function UniqueGen(s: string = "ScriptedEuch-") {
+    const gen = UniqueNameGenerator(s);
+    return () => gen.next().value;
+}
---
+++ src/utils/utils.test.ts
@@ -0,0 +1,12 @@
+import { expect, test } from "bun:test";
+import { UniqueGen } from "./index";
+
+test("UniqueGen Test", () => {
+    const g = UniqueGen("Test-");
+    const gen = UniqueGen("Test-");
+    
+    const unique0 = gen();
+    expect(unique0).toBe("Test-0");
+    const unique1 = gen();
+    expect(unique1).toBe("Test-1");
+});
