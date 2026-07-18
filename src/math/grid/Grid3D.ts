48aae1b2ee9e77d2e21beaf85b8e3478f9474844
Benjamin Zaporzan <benzaporzan@gmail.com>
2026-07-18 12:52:52 -0400

    Untabify

--- src/math/grid/Grid3D.ts
+++ src/math/grid/Grid3D.ts
@@ -20,9 +20,9 @@
 
 class GridError extends Error {
     constructor(message: string) {
-	super(message);
-	this.name = "GridError";
-	Object.setPrototypeOf(this, GridError.prototype);
+        super(message);
+        this.name = "GridError";
+        Object.setPrototypeOf(this, GridError.prototype);
     }
 }
 
@@ -34,104 +34,104 @@ export default class Grid3D<T = number> {
     private depth: number;
 
     constructor(obj = {}) {
-	this.width = obj.width ?? 1;
-	this.height = obj.height ?? 1;
-	this.depth = obj.depth ?? 1;
-	this.sentinel = obj.sentinel ?? null;
-	this.forEachIndex((i, j, k) => {
-	    this.data.push(this.sentinel);
-	});
+        this.width = obj.width ?? 1;
+        this.height = obj.height ?? 1;
+        this.depth = obj.depth ?? 1;
+        this.sentinel = obj.sentinel ?? null;
+        this.forEachIndex((i, j, k) => {
+            this.data.push(this.sentinel);
+        });
 
-	if (this.width < 1) throw new GridError("Width cannot be less than 1");
-	if (this.height < 1) throw new GridError("Height cannot be less than 1");
-	if (this.depth < 1) throw new GridError("Depth cannot be less than 1");
+        if (this.width < 1) throw new GridError("Width cannot be less than 1");
+        if (this.height < 1) throw new GridError("Height cannot be less than 1");
+        if (this.depth < 1) throw new GridError("Depth cannot be less than 1");
     }
 
     clone(): Grid3D<T> {
-	let g = new Grid3D({
-	    width: this.width,
-	    height: this.height,
-	    depth: this.depth,
-	    sentinel: this.sentinel,
-	});
-	this.forEachIndex((i, j, k) => {
-	    const v = this.getAt(i, j, k);
-	    g.setAt(i, j, k, v);
-	});
-	return g;
+        let g = new Grid3D({
+            width: this.width,
+            height: this.height,
+            depth: this.depth,
+            sentinel: this.sentinel,
+        });
+        this.forEachIndex((i, j, k) => {
+            const v = this.getAt(i, j, k);
+            g.setAt(i, j, k, v);
+        });
+        return g;
     }
 
     size(): number {
-	return this.width * this.height * this.depth;
+        return this.width * this.height * this.depth;
     }
 
     index(x: number, y: number, z: number = 0): number {
-	let array_index = this.width * (this.height * z + y) + x;
-	return array_index;
+        let array_index = this.width * (this.height * z + y) + x;
+        return array_index;
     }
 
     forEachIndex(f): void {
-	for (let k = 0; k < this.depth; k++) {
-	    for (let j = 0; j < this.height; j++) {
-		for (let i = 0; i < this.width; i++) {
-		    f.bind(this)(i, j, k);
-		}
-	    }
-	}
+        for (let k = 0; k < this.depth; k++) {
+            for (let j = 0; j < this.height; j++) {
+                for (let i = 0; i < this.width; i++) {
+                    f.bind(this)(i, j, k);
+                }
+            }
+        }
     }
 
     getAt(i: number, j: number, k: number = 0): T {
-	if (i < 0) throw new GridError("'i' index is out of bounds. i < 0.");
-	if (i >= this.width) throw new GridError("'i' index is out of bounds. i >= depth");
-	if (j < 0) throw new GridError("'j' index is out of bounds. j < 0.");
-	if (j >= this.height) throw new GridError("'j' index is out of bounds. j >= height");
-	if (k < 0) throw new GridError("'k' index is out of bounds. k < 0.");
-	if (k >= this.depth) throw new GridError("'k' index is out of bounds. k >= depth");
-	return this.data[this.index(i, j, k)];
+        if (i < 0) throw new GridError("'i' index is out of bounds. i < 0.");
+        if (i >= this.width) throw new GridError("'i' index is out of bounds. i >= depth");
+        if (j < 0) throw new GridError("'j' index is out of bounds. j < 0.");
+        if (j >= this.height) throw new GridError("'j' index is out of bounds. j >= height");
+        if (k < 0) throw new GridError("'k' index is out of bounds. k < 0.");
+        if (k >= this.depth) throw new GridError("'k' index is out of bounds. k >= depth");
+        return this.data[this.index(i, j, k)];
     }
 
     getAtIndex(idx: number): T {
-	return this.data[idx];
+        return this.data[idx];
     }
 
     setAt(i: number, j: number, k: number, value: T): void {
-	this.data[this.index(i, j, k)] = value;
+        this.data[this.index(i, j, k)] = value;
     }
 
     locateElement(x: number, y: number, z: number): T {
-	x = (x >= 0) ? x : 0;
-	x = (x <= this.width()-1) ? x : this.width()-1;
-	x = Math.floor(x);
+        x = (x >= 0) ? x : 0;
+        x = (x <= this.width()-1) ? x : this.width()-1;
+        x = Math.floor(x);
 
-	y = (y >= 0) ? y : 0;
-	y = (y <= this.height()-1) ? y : this.height()-1;
-	y = Math.floor(y);
+        y = (y >= 0) ? y : 0;
+        y = (y <= this.height()-1) ? y : this.height()-1;
+        y = Math.floor(y);
 
-	z = (z !== undefined) ? z : 0;
-	z = (z >= 0) ? z : 0;
-	z = (z <= this.depth()-1) ? z : this.depth()-1;
-	z = Math.floor(z);
+        z = (z !== undefined) ? z : 0;
+        z = (z >= 0) ? z : 0;
+        z = (z <= this.depth()-1) ? z : this.depth()-1;
+        z = Math.floor(z);
 
-	let element = this.getAt(x, y, z);
-	return element;
+        let element = this.getAt(x, y, z);
+        return element;
     }
 
     subGrid(obj): SubGrid {
-	const {
-	    x,
-	    y,
-	    z = 0,
-	    width = 1,
-	    height = 1,
-	    depth = 1,
-	} = obj;
-	return new SubGrid({
-	    parent: this,
-	    x, y, z,
-	    width,
-	    height,
-	    depth,
-	});
+        const {
+            x,
+            y,
+            z = 0,
+            width = 1,
+            height = 1,
+            depth = 1,
+        } = obj;
+        return new SubGrid({
+            parent: this,
+            x, y, z,
+            width,
+            height,
+            depth,
+        });
     }
 }
 
@@ -150,48 +150,48 @@ export class SubGrid<T = number> {
     private depth: number;
 
     constructor(obj = {}) {
-	this.parent = obj.parent;
-	this.x = obj.x;
-	this.y = obj.y;
-	this.z = obj.z ?? 0;
-	this.width = obj.width ?? 1;
-	this.height = obj.height ?? 1;
-	this.depth = obj.depth ?? 1;
+        this.parent = obj.parent;
+        this.x = obj.x;
+        this.y = obj.y;
+        this.z = obj.z ?? 0;
+        this.width = obj.width ?? 1;
+        this.height = obj.height ?? 1;
+        this.depth = obj.depth ?? 1;
     }
 
     getAt(i: number, j: number, k: number = 0): T {
-	return this.parent.getAt(i + this.x,
-				 j + this.y,
-				 k + this.z);
+        return this.parent.getAt(i + this.x,
+                                 j + this.y,
+                                 k + this.z);
     }
 
     setAt(i, j, k, value): void {
-	this.parent.setAt(i + this.x,
-			  j + this.y,
-			  k + this.z);
+        this.parent.setAt(i + this.x,
+                          j + this.y,
+                          k + this.z);
     }
 
     size(): number {
-	return this.width * this.height * this.depth;
+        return this.width * this.height * this.depth;
     }
 
     index(i, j, k): number {
-	return this.parent.index(
-	    i + this.x,
-	    j + this.y,
-	    k + this.z,
-	);
+        return this.parent.index(
+            i + this.x,
+            j + this.y,
+            k + this.z,
+        );
     }
 
     // Represents the SubGrid Local Index.
     forEachIndex(f): void {
-	for (let k = 0; k < this.depth; k++) {
-	    for (let j = 0; j < this.height; j++) {
-		for (let i = 0; i < this.width; i++) {
-		    f.bind(this)(i, j, k);
-		}
-	    }
-	}
+        for (let k = 0; k < this.depth; k++) {
+            for (let j = 0; j < this.height; j++) {
+                for (let i = 0; i < this.width; i++) {
+                    f.bind(this)(i, j, k);
+                }
+            }
+        }
     }
 
     //
@@ -200,19 +200,19 @@ export class SubGrid<T = number> {
 
     // Represents the Grid3D Global Index
     forEachGlobalIndex(f): void {
-	this.forEachIndex((i, j, k) => {
-	    f.bind(this)(i+this.i, j+this.j, k+this.k);
-	});
+        this.forEachIndex((i, j, k) => {
+            f.bind(this)(i+this.i, j+this.j, k+this.k);
+        });
     }
 
     /**
        SubGrid Lens Functions (1x1 Grid)
      */
     get(): T {
-	return this.getAt(0, 0, 0);
+        return this.getAt(0, 0, 0);
     }
 
     set(v = null): void {
-	this.setAt(0, 0, 0, v);
+        this.setAt(0, 0, 0, v);
     }
 }
--- src/math/grid/PathFinding.ts
+++ src/math/grid/PathFinding.ts
@@ -18,67 +18,67 @@ export default class PathFinding<T> {
 	} = obj;
 	let path_grid = this.generatePathGrid();
 	let starting_node = path_grid.getAt(x, y, z);
-	starting_node.sentinel = 0;
-	this.calculateCost(path_grid, x, y, z)
-	let unvisited_nodes = [];
-	do {
-	    unvisited_nodes = path_grid.data.filter((element) => {
-		return !element.visited && element.sentinel !== null;
-	    });
-	    unvisited_nodes.forEach((element) => this.calculateCost(
-		path_grid, element.x, element.y, element.z, costFunction));
-	} while (unvisited_nodes.length > 0);
-	return path_grid;
+        starting_node.sentinel = 0;
+        this.calculateCost(path_grid, x, y, z)
+        let unvisited_nodes = [];
+        do {
+            unvisited_nodes = path_grid.data.filter((element) => {
+                return !element.visited && element.sentinel !== null;
+            });
+            unvisited_nodes.forEach((element) => this.calculateCost(
+                path_grid, element.x, element.y, element.z, costFunction));
+        } while (unvisited_nodes.length > 0);
+        return path_grid;
     }
 
     private generatePathGrid(): Grid3D<PathElement> {
-	let g = new Grid3D<PathElement>({
-	    width: this.base_grid.width,
-	    height: this.base_grid.height,
-	    depth: this.base_grid.depth,
-	    sentinel: null,
-	});
-	g.forEachIndex((i, j, k) => {
-	    g.setAt(i, j, k, new PathElement({
-		path_grid: g,
-		x: i,
-		y: j,
-		z: k,
-	    }));
-	});
-	return g;
+        let g = new Grid3D<PathElement>({
+            width: this.base_grid.width,
+            height: this.base_grid.height,
+            depth: this.base_grid.depth,
+            sentinel: null,
+        });
+        g.forEachIndex((i, j, k) => {
+            g.setAt(i, j, k, new PathElement({
+                path_grid: g,
+                x: i,
+                y: j,
+                z: k,
+            }));
+        });
+        return g;
     }
 
     private calculateCost(path_grid: Grid3D<PathElement>,
-			  x: number,
-			  y: number,
-			  z: number,
-			  costFunction: (PathFinding, PathElement, PathElement) => number,
-			 ): void {
-	let element = path_grid.getAt(x, y, z);
-	element.visited = true;
+                          x: number,
+                          y: number,
+                          z: number,
+                          costFunction: (PathFinding, PathElement, PathElement) => number,
+                         ): void {
+        let element = path_grid.getAt(x, y, z);
+        element.visited = true;
 
-	const compFunction = (e1: PathElement, e2: PathElement) => {
-	    if (e2.visited) return;
-	    let traversal_cost = (e1.sentinel ?? 0) + costFunction(this, e1, e2);
-	    if (traversal_cost <= (e2.sentinel ?? 0)) {
-		e2.sentinel = traversal_cost;
-		e2.parent = e1;
-	    }
-	}
+        const compFunction = (e1: PathElement, e2: PathElement) => {
+            if (e2.visited) return;
+            let traversal_cost = (e1.sentinel ?? 0) + costFunction(this, e1, e2);
+            if (traversal_cost <= (e2.sentinel ?? 0)) {
+                e2.sentinel = traversal_cost;
+                e2.parent = e1;
+            }
+        }
 
-	if (element.top() !== null)
-	    compFunction(element, element.top());
-	if (element.bottom() !== null)
-	    compFunction(element, element.bottom());
-	if (element.up() !== null)
-	    compFunction(element, element.up());
-	if (element.right() !== null)
-	    compFunction(element, element.right());
-	if (element.down() !== null)
-	    compFunction(element, element.down());
-	if (element.left() !== null)
-	    compFunction(element, element.left());
+        if (element.top() !== null)
+            compFunction(element, element.top());
+        if (element.bottom() !== null)
+            compFunction(element, element.bottom());
+        if (element.up() !== null)
+            compFunction(element, element.up());
+        if (element.right() !== null)
+            compFunction(element, element.right());
+        if (element.down() !== null)
+            compFunction(element, element.down());
+        if (element.left() !== null)
+            compFunction(element, element.left());
     }
 }
 
@@ -94,40 +94,40 @@ class PathElement {
     z: number;
 
     constructor(obj = {}) {
-	this.path_grid = obj.path_grid;
-	this.x = obj.x;
-	this.y = obj.y;
-	this.z = obj.z;
+        this.path_grid = obj.path_grid;
+        this.x = obj.x;
+        this.y = obj.y;
+        this.z = obj.z;
     }
 
     top(): PathElement|null {
-	if (this.z >= this.path_grid.depth) return null;
-	return this.path_grid.getAt(this.x, this.y, this.z+1);
+        if (this.z >= this.path_grid.depth) return null;
+        return this.path_grid.getAt(this.x, this.y, this.z+1);
     }
 
     bottom(): PathElement|null {
-	if (this.z <= 0) return null;
-	return this.path_grid.getAt(this.x, this.y, this.z-1);
+        if (this.z <= 0) return null;
+        return this.path_grid.getAt(this.x, this.y, this.z-1);
     }
 
     up(): PathElement|null {
-	if (this.y >= this.path_grid.height) return null;
-	return this.path_grid.getAt(this.x, this.y+1, this.z);
+        if (this.y >= this.path_grid.height) return null;
+        return this.path_grid.getAt(this.x, this.y+1, this.z);
     }
 
     right(): PathElement|null {
-	if (this.x >= this.path_grid.width) return null;
-	return this.path_grid.getAt(this.x+1, this.y, this.z);
+        if (this.x >= this.path_grid.width) return null;
+        return this.path_grid.getAt(this.x+1, this.y, this.z);
     }
 
     down(): PathElement|null {
-	if (this.y <= 0) return null;
-	return this.path_grid.getAt(this.x, this.y-1, this.z);
+        if (this.y <= 0) return null;
+        return this.path_grid.getAt(this.x, this.y-1, this.z);
     }
 
     left(): PathElement|null {
-	if (this.x <= 0) return null;
-	return this.path_grid.getAt(this.x-1, this.y, this.z);
+        if (this.x <= 0) return null;
+        return this.path_grid.getAt(this.x-1, this.y, this.z);
     }
 }
 
