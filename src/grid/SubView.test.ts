30bd0480263ee31c402c2fa3708ee768a66319d8
Benjamin Zaporzan <benzaporzan@gmail.com>
2026-07-30 13:22:30 -0400

    Last Update

--- src/grid/SubView.test.ts
+++ src/grid/SubView.test.ts
@@ -4,7 +4,7 @@ import Grid3D from "./Grid3D";
 import { SeededRandomNumberGenerator } from "../random";
 import { Display2D } from "./utils";
 
-test("SubVie Init 1", () => {
+test("SubView Init 1", () => {
     let g = new Grid3D({ width: 50, height: 50, sentinel: 0 });
     let srng = new SeededRandomNumberGenerator("test");
     let sv = g.subView();
@@ -12,8 +12,8 @@ test("SubVie Init 1", () => {
     let sg = g.subGrid({
 	x: srng.randomInteger(1, 25),
 	y: srng.randomInteger(1, 25),
-	width: srng.randomInteger(12, 24),
-	height: srng.randomInteger(12, 24),
+	width: srng.randomInteger(4, 14),
+	height: srng.randomInteger(4, 14),
     });
 
     sv.insertGrid(sg);
@@ -26,7 +26,7 @@ test("SubView Init 2", () => {
     let srng = new SeededRandomNumberGenerator();
     let sv = g.subView();
     const MAX_CYCLES = 1000;
-    const MAX_NUM_UNIONS = 12;
+    const MAX_NUM_UNIONS = 16;
     let num_unions = 0;
     let sg_listing = [];
     for (let cycle = 0; cycle < MAX_CYCLES; cycle++) {
@@ -34,8 +34,8 @@ test("SubView Init 2", () => {
 	const dimensions = {
 	    x: srng.randomInteger(1, 25),
 	    y: srng.randomInteger(1, 25),
-	    width: srng.randomInteger(8, 14),
-	    height: srng.randomInteger(8, 14),
+	    width: srng.randomInteger(3, 24),
+	    height: srng.randomInteger(3, 24),
 	};
 	if (dimensions.x + dimensions.width > g.width) continue;
 	if (dimensions.y + dimensions.height > g.height) continue;
